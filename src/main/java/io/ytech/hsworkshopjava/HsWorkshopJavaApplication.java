00f1d9006132570e6c4f13cad472bd64c607620d
Lourenco Catarino <lourenco.catarino@asesormasmovil.es>
2022-05-07 15:48:56 +0100

    init

---
+++ src/main/java/io/ytech/hsworkshopjava/HsWorkshopJavaApplication.java
@@ -0,0 +1,13 @@
+package io.ytech.hsworkshopjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsWorkshopJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsWorkshopJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ytech/hsworkshopjava/HsWorkshopJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ytech.hsworkshopjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsWorkshopJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
